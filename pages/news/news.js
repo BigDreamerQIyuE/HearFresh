96fbbe35f309a9ad95fe68e47f878a289d2be42b
BigDreamer <1256515509@qq.com>
2018-07-25 12:33:38 +0800

    news页下拉刷新，上拉加载更多，改进评论图标

--- pages/news/news.js
+++ pages/news/news.js
@@ -1,13 +1,14 @@
 // pages/news/news.js
 var app = getApp(),
   page = 1,
-  pageSize = 14
+  pageSize = 5
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    reachBottom: false,
     headLine: [{
       id: '',
       title: '',
@@ -145,8 +146,70 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-   /* page++
-    console.log("page:"+page+"pageSize:"+pageSize)
+    page = 1
+    this.data.article.splice(0, this.data.article.length);
+    this.data.headLine.splice(0, this.data.headLine.length);
+    this.setData({
+      article: this.data.article,
+      headLine: this.data.headLine,
+      reachBottom:false
+    })
+
+    var _this = this
+    wx.request({
+      url: 'http://139.199.79.232/HearFresh/GetTheNewsList.php',
+      method: 'POST',
+      data: {
+        page: 1,
+        pageSize: pageSize
+      },
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+
+      success: function(res) {
+        wx.stopPullDownRefresh()
+        _this.setData({
+          'headLine[0].id': res.data.data.headLine.headLineId,
+          'headLine[0].title': res.data.data.headLine.title,
+          'headLine[0].cover': res.data.data.headLine.cover,
+          'headLine[0].description': res.data.data.headLine.description,
+        })
+        for (var i = 0; i < res.data.data.news.length; i++) {
+          var param = {};
+          var string = "article[" + i + "].id";
+          param[string] = res.data.data.news[i].newsId;
+
+          var string = "article[" + i + "].title";
+          param[string] = res.data.data.news[i].title;
+
+
+          var string = "article[" + i + "].time";
+          param[string] = res.data.data.news[i].createdAt.date;
+
+
+          var string = "article[" + i + "].read";
+          param[string] = res.data.data.news[i].reading;
+
+
+          var string = "article[" + i + "].cover";
+          param[string] = res.data.data.news[i].cover;
+          _this.setData(param);
+        }
+      },
+      fail: function(e) {
+        wx.showActionSheet({
+          itemList: ["fuck,connection fail"],
+        })
+      }
+    })
+  },
+
+
+
+  onReachBottom: function() {
+    page++
+    console.log("page:" + page + "pageSize:" + pageSize)
 
     var _this = this
     wx.request({
@@ -161,36 +224,42 @@ Page({
       },
 
       success: function(res) {
-        console.log(res.data);
-        var fuck=(page-1)*3-1
-        for (var i = 0; i < res.data.data.news.length; i++) {
-          var param = {};
-          var string = "article[" + fuck + "].id";
-          param[string] = res.data.data.news[i].newsId;
-          // console.log(res.data.data.news[i].newsId)
-          //_this.setData(param);
+        if (res.data.data.news.length == 0) {
+          _this.setData({
+            reachBottom: true
+          })
+          page--
+        } else {
+          console.log(res.data);
+          var fuck = (page - 1) * 5 - 1
+          for (var i = 0; i < res.data.data.news.length; i++, fuck++) {
+            var param = {};
+            var string = "article[" + fuck + "].id";
+            param[string] = res.data.data.news[i].newsId;
+            // console.log(res.data.data.news[i].newsId)
+            //_this.setData(param);
 
-          var string = "article[" + fuck + "].title";
-          param[string] = res.data.data.news[i].title;
-          // console.log(res.data.data.news[i].title)
-          // _this.setData(param);
+            var string = "article[" + fuck + "].title";
+            param[string] = res.data.data.news[i].title;
+            // console.log(res.data.data.news[i].title)
+            // _this.setData(param);
 
-          var string = "article[" + fuck + "].time";
-          param[string] = res.data.data.news[i].createdAt.date;
-          //console.log(res.data.data.news[i].createdAt.data)
-          // _this.setData(param);
+            var string = "article[" + fuck + "].time";
+            param[string] = res.data.data.news[i].createdAt.date;
+            //console.log(res.data.data.news[i].createdAt.data)
+            // _this.setData(param);
 
-          var string = "article[" + fuck + "].read";
-          param[string] = res.data.data.news[i].reading;
-          //console.log(res.data.data.news[i].reading)
-          //_this.setData(param);
+            var string = "article[" + fuck + "].read";
+            param[string] = res.data.data.news[i].reading;
+            //console.log(res.data.data.news[i].reading)
+            //_this.setData(param);
 
-          var string = "article[" + fuck + "].cover";
-          param[string] = res.data.data.news[i].cover;
-          // console.log(res.data.data.news[i].cover)
-          _this.setData(param);
+            var string = "article[" + fuck + "].cover";
+            param[string] = res.data.data.news[i].cover;
+            // console.log(res.data.data.news[i].cover)
+            _this.setData(param);
+          }
         }
-
       },
       fail: function(e) {
         wx.showActionSheet({
@@ -198,13 +267,6 @@ Page({
         })
       }
     });
-*/
-  },
-
-
-
-  onReachBottom: function() {
-
 
   },
 
